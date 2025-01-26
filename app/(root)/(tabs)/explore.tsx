0805e1de9c716dc33469a477586e23f587a039df
Suny Dutta <sanidutta6@gmail.com>
2025-01-26 08:57:47 +0530

    updated screens

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,11 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Card, FeaturedCard } from "@/components/Card";
+import Filters from "@/components/Filters";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import { router } from "expo-router";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Explore = () => {
     return (
-        <View>
-            <Text>Explore</Text>
-        </View>
+        <SafeAreaView className="bg-white h-full">
+            <FlatList
+                data={[1, 2, 3, 4, 5, 6]}
+                keyExtractor={(item) => item.toString()}
+                numColumns={2}
+                contentContainerClassName="pb-32"
+                columnWrapperClassName="flex gap-5 px-5"
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item }) => <Card onPress={() => router.push("/properties/1")} />}
+                ListHeaderComponent={
+                    <View className="px-5">
+                        <View className="flex flex-row items-center justify-between mt-5 ">
+                            <TouchableOpacity
+                                className="flex flex-row items-center justify-center bg-primary-200 rounded-full size-11"
+                                onPress={() => router.back()}
+                            >
+                                <Image source={icons.backArrow} className="size-6" />
+                            </TouchableOpacity>
+                            <Text className="text-base mr-2 text-center font-rubik-medium text-black-300">Search for Your Ideal Home</Text>
+                            <Image source={icons.bell} className="h-6 w-6" />
+                        </View>
+                        <Search />
+                        <View className="mt-5">
+                            <Filters />
+                            <Text className="text-xl font-rubik-bold text-black-300 mt-5">Found 5 Properties</Text>
+                        </View>
+                    </View>
+                }
+            />
+
+        </SafeAreaView>
     )
 }
 
--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,20 +1,70 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Card, FeaturedCard } from "@/components/Card";
+import Filters from "@/components/Filters";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { router } from "expo-router";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-3xl font-rubik my-10">Welcome to ReState</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Properties</Link>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={[1, 2]}
+        keyExtractor={(item) => item.toString()}
+        numColumns={2}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => <Card onPress={() => router.push("/properties/1")} />}
+        ListHeaderComponent={
+          <View className="px-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <View className="flex flex-row items-center">
+                <Image source={images.avatar} className="size-12 rounded-full" />
+                <View className="flex flex-col items-start justify-center ml-2">
+                  <Text className="text-xs font-rubik text-black-100">Good Morning,</Text>
+                  <Text className="text-base font-rubik-medium text-black-300">Adrian</Text>
+                </View>
+              </View>
+              <Image source={icons.bell} className="size-6" />
+            </View>
+
+            <Search />
+
+            <View className="my-5">
+              <View className="flex flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+                <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+                </TouchableOpacity>
+              </View>
+              <FlatList
+                data={[1, 2, 3]}
+                renderItem={({ item }) => <FeaturedCard onPress={() => router.push("/properties/1")} />}
+                keyExtractor={(item) => item.toString()}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                bounces={false}
+                contentContainerClassName="flex gap-5 mt-5"
+              />
+            </View>
+
+            <View className="my-5">
+              <View className="flex flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
+                <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            <Filters />
+          </View>
+        }
+      />
+
+    </SafeAreaView>
   );
 }
